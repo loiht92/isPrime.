f2fe2c587ea0dc7f41a3bd0d6b03c822234e7629
loiminh <loiho11082015@gmail.com>
2020-02-14 16:46:50 +0700

    add file

---
+++ src/CheckPrime.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CheckPrime {
+    public static void main(String[] args) {
+
+        for (int j = 1; j < 100; j++) {
+            if (isPrime(j) == true){
+                System.out.println(j + " ");
+            }
+
+        }
+    }
+
+    private static boolean isPrime(int x) {
+        if (x < 2) return false;
+        for (int i = 2; i < x - 1; i++) {
+            if (x % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+}
+
